439baf12fa03916534172395841f52ed0bc5ed5f
Sailaxmi <sailaxmi.kasarla9@gmail.com>
2022-02-11 16:57:31 +0000

    Commit by Sailaxmi

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,8 +5,6 @@ public class HelloWorld {
 		System.out.println("Hello World...................!, its my first class");
 System.out.println("This is the second line in java class");
 System.out.println("This line added by sailaxmi");
-System.out.println("Added after git reset --soft HAED^");
-System.out.println("Added line after soft reset");
 	}
 
 }
