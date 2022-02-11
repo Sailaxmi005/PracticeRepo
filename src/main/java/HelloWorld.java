a65999e3fa5b907c6d2f39d108646124073c9553
Sailaxmi <sailaxmi.kasarla9@gmail.com>
2022-02-11 12:11:38 +0000

    added line after reset soft and new  file

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 System.out.println("This is the second line in java class");
 System.out.println("This line added by sailaxmi");
 System.out.println("Added after git reset --soft HAED^");
+System.out.println("Added line after soft reset");
 	}
 
 }
