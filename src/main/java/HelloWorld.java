c43f1ba6413dcd171da4f216ae545c2795c1123e
Sailaxmi <sailaxmi.kasarla9@gmail.com>
2022-02-11 16:36:06 +0000

    Commit by Developer branch

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World...................!, its my first class");
 System.out.println("This is the second line in java class");
-System.out.println("This line added by sailaxmi");
+System.out.println("This line added by developer");
 System.out.println("Added after git reset --soft HAED^");
 System.out.println("Added line after soft reset");
 	}
