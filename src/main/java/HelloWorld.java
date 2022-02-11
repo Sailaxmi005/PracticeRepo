63644e4340bff73c6f055e67a1df3626c55fa967
Sailaxmi005 <96123519+Sailaxmi005@users.noreply.github.com>
2022-02-11 16:02:26 +0000

    sdfghjllkjhgf

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,15 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World...................!, its my first class");
 System.out.println("This is the second line in java class");
-<<<<<<< HEAD
 System.out.println("This line is check out by Testing branch");
 System.out.println("This line edited by Testing......");
-=======
-
-System.out.println("This line is check out by Testing branch");
-System.out.println("This line edited by Testing......");
-
->>>>>>> 593c75cf3ef3d45589fd86591ecb37da25425164
 	}
 
 }
